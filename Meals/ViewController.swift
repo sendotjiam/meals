2a348f8b397553fdc96c95553de5e8e93042c720
Sendo Tjiamis <sendotjiamis9898@gmail.com>
2023-09-22 13:26:46 +0700

    Initial Commit

---
+++ Meals/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meals
+//
+//  Created by Sendo Tjiam on 22/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
